cda56c36bad56470831dbb3d057b7cdb182c2a30
hetj2004 <het28jasani.com>
2025-01-28 10:59:37 -0500

    updated code

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,24 +1,25 @@
-﻿using COMP2139_Labs.Models;
+﻿using COMP2139_Labs.Data;
+using COMP2139_Labs.Models;
 using comp2139.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace COMP2139_Labs.Controllers;
 
 public class ProjectController : Controller
 {
-    /// <summary>
-    /// Index action will retrieve a listing of projects (database)
-    /// </summary>
-    /// <returns></returns>
+
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
     [HttpGet]
     public IActionResult Index()
     {
-        //Database --> Retrieve project from database
-        var projects = new List<Project>()
-        {
-            new Project { ProjectId = 1, Name = "Project 1", Description = "First Project", }
-            //feel free to add more projects here
-        }; 
+        var projects = _context.Projects.ToList();
         return View(projects);
     }
     
@@ -29,21 +30,95 @@ public class ProjectController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult Create(Project project)
     {
-        //Database --> Retrieve project from database
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+            _context.Projects.Add(project);      //Add proect to database in memory
+            _context.SaveChanges();             //Save changes (persist/commit) to database
+            return RedirectToAction("Index");  //Redirect to the  (ProjectController) Index action
+        }
+        return View(project);
     }
     
-    //CRUD - Create - Read - Update - Delete
     
     [HttpGet]
-
     public IActionResult Details(int id)
     {
-        //Database --> Retrieve project from database
-        var project = new Project { ProjectId = id, Name = "Project" + id, Description = "Details of Project" + id};
+        //Retrieves the project with the ProjectId specified of returns null if not found
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();  //Returns a 404 error if the project is not found
+        }
         return View(project);
     }
 
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var project = _context.Projects.Find(id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return View(project);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, [Bind("ProjectId, Name, Description")] Project project)
+    {
+        if (id != project.ProjectId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Projects.Update(project);  //Update the Project
+                _context.SaveChanges();             //Commit changes to database
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!ProjectExists(project.ProjectId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;  //throws error if the exception cannot be identified (unknown)
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        return View(project);
+    }
+
+    /// <summary>
+    /// Checks if project exists in the database
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    private bool ProjectExists(int id)
+    {
+        return _context.Projects.Any(e => e.ProjectId == id);
+    }
+
+    
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return View(project);
+    }
+    
 }
